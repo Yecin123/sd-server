f6e23f936f9be5906edf5f70d3b7d389cb93d340
Yecin123 <yecin0123@gmail.com>
2019-01-14 22:35:38 +0100

    First commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+using System.IO;
+
+using System.Text;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController 
+    {
+
+
+        private string username = "admin";
+        private string password = "admin";
+        // POST api/values
+        [HttpPost]
+        public string Post([FromBody] IReadOnlyDictionary<string,string> value)
+        {
+
+            var username = value.GetValueOrDefault("username",null);
+            var password = value.GetValueOrDefault("password",null);
+
+            if (this.username.Equals(username) && this.password.Equals(password))
+                    return SuccessResponse();
+            return ErrorResponse();
+
+
+
+        }
+
+
+        private string SuccessResponse() {
+
+             string filePath = Path.Combine( Directory.GetCurrentDirectory(), "wwwroot");
+
+            var fileTarget  = filePath + "/success.html";
+
+           string response = File.ReadAllText(fileTarget, Encoding.UTF8);
+
+            return response;
+        }
+
+        private string ErrorResponse() {
+
+
+
+             string filePath = Path.Combine( Directory.GetCurrentDirectory(), "wwwroot");
+
+            var fileTarget  = filePath + "/error.html";
+
+           string response = File.ReadAllText(fileTarget, Encoding.UTF8);
+
+            return response;
+        }
+
+
+        [HttpGet]
+        public String Get() {
+
+
+            return "test server working successfull ";
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+           .UseKestrel()
+           .UseContentRoot(Directory.GetCurrentDirectory())
+           .UseUrls("https://localhost:3001", "http://127.0.0.1:3000", "https://:::3000")
+           .UseIISIntegration()
+           .UseStartup<Startup>()
+           .UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot") )
+            .Build();
+
+           host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
